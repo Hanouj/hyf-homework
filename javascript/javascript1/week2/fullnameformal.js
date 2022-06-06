cd7fe2a53a00b0bd5e5aad13d170fdce6c429502
Hanouj <hannaremy@outlook.com>
2022-06-06 23:31:19 +0200

    changes in fullnameFormal

--- javascript/javascript1/week2/fullnameformal.js
+++ javascript/javascript1/week2/fullnameformal.js
@@ -2,12 +2,16 @@ function getFullname (firstname, surname, useFormalName, gender){
 if (useFormalName) { 
     if (gender==="man") {
     console.log(`lord ${firstname} ${surname}`)   
+    return `Lord ${firstname} ${surname}`;
 } else if(gender==="woman") {
     console.log(`lady ${firstname} ${surname}`)
+    return `lady ${firstname} ${surname}`;
 }else{
     console.log(`${firstname} ${surname}`)
+    return `${firstname} ${surname}`;
 }   
 }
+
 }
 
 
@@ -18,6 +22,10 @@ getFullname ('Benjamin', 'Hughes', true, "man");
 
 const fullname1 = getFullname('Hanaa','Hussein', true, "woman");
 const fullname2 = getFullname('Sarah', 'Amouri',true);
+
+getFullname("", "");
+getFullname(true, true);
+
 
 
 // when I ran my code in codepan any name I write it give me undefiend as a result!
