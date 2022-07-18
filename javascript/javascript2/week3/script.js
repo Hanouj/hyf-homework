2269a35ae3dcb8237d30c80d66ee348241ce770f
Hanouj <hannaremy@outlook.com>
2022-07-18 19:45:28 +0200

    rewriting some codes and creating a new js and html file

--- javascript/javascript2/week3/script.js
+++ javascript/javascript2/week3/script.js
@@ -18,19 +18,14 @@ delyaedText();
 //2
 
 function loggingOutString(delay, stringTolog) {
-  return function () {
-    console.log(delay, stringTolog);
-  };
+  setTimeout(() => {
+    console.log(stringTolog);
+  }, delay * 1000);
 }
 
-const setForFiveSec = loggingOutString(5, "This string logged after 5 seconds");
+loggingOutString(5, "This string logged after 5 seconds");
 
-const setForThreeSec = loggingOutString(
-  3,
-  "This string logged after 3 seconds"
-);
-setForFiveSec();
-setForThreeSec();
+loggingOutString(3, "This string logged after 3 seconds");
 
 //3
 const btnShowText = document.getElementById("btn");
@@ -82,10 +77,20 @@ runAfterDelay(1, function () {
 });
 
 //8
-
-window.addEventListener("dblclick", (event) => {
-  setTimeout(() => event, 500);
-  console.log("Double-click detected");
+let mouseClickTimes = 0;
+window.addEventListener("click", (event) => {
+  mouseClickTimes++;
+  setTimeout(() => {
+    if (mouseClickTimes === 1) {
+      // handle for single click
+      console.log("Single click");
+    } else if (mouseClickTimes === 2) {
+      // handle for double click
+      console.log("Double click");
+    }
+    mouseClickTimes = 0;
+  }, 500);
+  return event;
 });
 
 //9
@@ -115,7 +120,7 @@ let functionArray = [
   secondFunction,
   thirdFunction,
   function () {
-    console.log("annonmyse function");
+    console.log("anonymous function");
   },
 ];
 
@@ -155,69 +160,9 @@ goodbye();
 const animal = {
   type: "cat",
   name: "Daisey",
-  sounds() {
+  sound: () => {
     console.log("meoww meow");
   },
 };
 
-animal.sounds();
-
-//3. The fastest presser in this realm
-
-const button = document.getElementById("start-btn");
-const inputValue = document.getElementById("seconds-input");
-let lPresses = 0;
-let sPresses = 0;
-const lScore = document.getElementById("l");
-const sScore = document.getElementById("s");
-let gameFinished = true;
-const countdownTimer = document.getElementById("countdown");
-
-button.addEventListener("click", () => {
-  gameFinished = false;
-  lPresses = 0;
-  sPresses = 0;
-  lScore.textContent = 0;
-  sScore.textContent = 0;
-  const seconds = inputValue.value;
-
-  if (seconds == "") {
-    alert("set seconds first");
-    console.log("Error");
-  } else {
-    setTimeout(() => {
-      console.log("time is up");
-      console.log(`we had l pressed ${lPresses}`);
-      console.log(`we had s pressed ${sPresses}`);
-
-      if (lPresses > sPresses) {
-        console.log("l wins");
-        const lWins = document.getElementById("l-winner");
-        lWins.textContent = "L wins";
-      } else if (sPresses > lPresses) {
-        console.log("s wins");
-        const sWins = document.getElementById("s-winner");
-        sWins.textContent = "S wins";
-      } else {
-        console.log("its a tie");
-        const itsAtie = document.getElementById("a-tie");
-        itsAtie.textContent = "its a tie";
-      }
-
-      gameFinished = true;
-    }, seconds * 1000);
-  }
-});
-
-document.addEventListener("keypress", (e) => {
-  if (!gameFinished) {
-    const letter = e.key;
-    if (letter === "l" || letter === "L") {
-      lPresses++;
-      lScore.textContent = lPresses;
-    } else if (letter === "s" || letter === "S") {
-      sPresses++;
-      sScore.textContent = sPresses;
-    }
-  }
-});
+animal.sound();
